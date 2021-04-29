8348bc3d6ee50de345dc59db7f95838f8cd1a27a
DennisWanjeri <shikandadennis07@gmail.com>
2021-04-29 10:46:23 +0000

    debug

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
+
+import sys
+
 if __name__ == "__main__":
-    import sys
     i = 1
     length = len(sys.argv) - 1
 
     if length == 0:
-        print("0 arguements.")
+        print("{:d} arguments.".format(length))
     elif length == 1:
-        print("{:d} arguement:".format(length))
+        print("{:d} argument:".format(length))
     else:
-        print("{:d} arguements:".format(length))
+        print("{:d} arguments:".format(length))
     while i <= length:
         print("{:d}: {:s}".format(i, sys.argv[i]))
         i += 1
