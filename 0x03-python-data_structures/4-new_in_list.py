92e2a6dcb9bb1b604eb85a0b924b76cfdb643611
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-04 16:06:08 +0000

    pep8

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return my_list
@@ -8,4 +9,3 @@ def new_in_list(my_list, idx, element):
         if i == idx:
             new_list[idx] = element
     return new_list
-            
