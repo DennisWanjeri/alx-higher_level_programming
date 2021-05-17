b6ba9ffb2efe787c2b08cb75049c46e13ad818f1
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-17 07:02:17 +0000

    safe division

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+
+    for elements in range(x):
+        try:
+            print("{:d}".format(my_list[elements]), end="")
+            i += 1
+        except (TypeError, ValueError):
+            continue
+    print()
+    return i
