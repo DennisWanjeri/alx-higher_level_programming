2ca2a2d80c79251e5068f75d0895aa1b587092ad
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-18 07:44:56 +0000

    debug

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -7,18 +7,22 @@ class Square:
     def __init__(self, size=0):
         """square initialization"""
         self.size = size
+
     @property
     def size(self):
         """returns current size of the square"""
         return (self.__size)
+
     @size.setter
     def size(self, value):
+        """validates value"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
         """area of square"""
         return (self.__size * self.__size)
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -7,21 +7,26 @@ class Square:
     def __init__(self, size=0):
         """square initialization"""
         self.size = size
+
     @property
     def size(self):
         """returns current size of the square"""
         return (self.__size)
+
     @size.setter
     def size(self, value):
+        """validating size"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
+
     def area(self):
         """area of square"""
         return (self.__size * self.__size)
+
     def my_print(self):
         """print square to stdout in # character"""
         for x in range(0, self.__size):
