ff1fcd8d8aff879b5d23aae35ac6a738e287f183
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-18 19:00:47 +0000

    debug, bug i > a

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -5,7 +5,7 @@ def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
         try:
-            if a > i:
+            if i > a:
                 raise Exception('Too far')
             else:
                 result += a ** b / i
---
+++ 0x05-python-exceptions/102-main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+magic_calculation = __import__('102-magic_calculation').magic_calculation
+
+result = magic_calculation(2,3)
+print(result)
+
+result = magic_calculation(5, 2)
+print(result)
