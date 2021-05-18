267e9facac94941bcf86ca97f115286f89086f47
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-18 06:24:28 +0000

    print square

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -15,10 +15,17 @@ class Square:
     def size(self, value):
         if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         else:
             self.__size = value
     def area(self):
         """area of square"""
         return (self.__size * self.__size)
+    def my_print(self):
+        """print square to stdout in # character"""
+        for x in range(0, self.__size):
+            [print("#", end="") for y in range(self.__size)]
+            print("")
+        if self.__size == 0:
+            print("")
