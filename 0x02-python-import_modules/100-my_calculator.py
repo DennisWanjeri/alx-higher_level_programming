2ca4ced69c741a6d4a583143ff449fed2494683f
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-19 17:27:11 +0000

    debug

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
+
 
 if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+
     if len(sys.argv)!= 4:
         print("usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
@@ -13,11 +15,5 @@ if __name__ == "__main__":
     a = int(sys.argv[1])
     b = int(sys.argv[3])
 
-    if sys.argv[2] == '+':
-        print("{} + {} = {}".format(a, b, add(a, b)))
-    elif sys.argv[2] == '-':
-        print("{} - {} = {}".format(a, b, sub(a, b)))
-    elif sys.argv[2] == '*':
-        print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif sys.argv[2] == '/':
-        print("{} / {} = {}".format(a, b, div(a, b)))
+
+    print("{} {} {} = {}".format(a, sys.argv[2], b, ops[sys.argv[2]](a, b)))
