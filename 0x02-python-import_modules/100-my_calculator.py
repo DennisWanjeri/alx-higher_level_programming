22d2bf896b4db0b6dca8009fea7925e237da3ec3
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-19 17:46:48 +0000

    debug

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import sys
 
     if len(sys.argv) != 4:
-        print("usage: ./100-my_calculator.py <a> <operator> <b>")
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
     ops = {"+": add, "-": sub, "*": mul, "/": div}
     if sys.argv[2] not in list(ops.keys()):
