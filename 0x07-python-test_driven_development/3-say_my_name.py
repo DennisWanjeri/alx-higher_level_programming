1907622cd4bd943a461ecbe72b7159592894e836
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-20 18:16:31 +0000

    say_my_name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""prints My name is <first name> <last name>"""
+
+
+def say_my_name(first_name, last_name=""):
+    """prints my full name"""
+    if type(first_name) != str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) != str:
+        raise TypeError("last_name must be a string")
+    if last_name == "":
+        print("My name is {}".format(first_name))
+    else:
+        print("My name is {} {}".format(first_name, last_name))
