459fef6751289c02735f8155d10bc6689ed2944e
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-20 18:20:56 +0000

    test driven dev say_my_name

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -8,7 +8,4 @@ def say_my_name(first_name, last_name=""):
         raise TypeError("first_name must be a string")
     elif type(last_name) != str:
         raise TypeError("last_name must be a string")
-    if last_name == "":
-        print("My name is {}".format(first_name))
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
