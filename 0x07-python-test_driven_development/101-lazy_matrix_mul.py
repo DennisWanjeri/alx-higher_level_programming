9728a9ea0b0900c61f9276613c812e6b935eaaa4
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-21 19:06:11 +0000

    TDD

---
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""matrix multiplication using Numpy"""
+import numpy as np
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """matrix multiplication using numpy"""
+    return (np.matmul(m_a, m_b))
