d1942d502d158fb545ec6c5320ae8c2ca0e7cf0e
DennisWanjeri <shikandadennis07@gmail.com>
2021-05-24 17:27:20 +0000

    rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,38 +1,75 @@
 #!/usr/bin/python3
-"""class Rectangle"""
+"""
+    1-rectangle: class Rectangle
+"""
 
 
 class Rectangle:
-    """Rectangle definition"""
+    """
+        class Rectangle defines a rectangle
+        Attributes:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+    """
     def __init__(self, width=0, height=0):
-        """constructor"""
-        self.width = width
-        self.height = height
+        """
+            initialises the instances
+            Args:
+                width (int): width of the rectangle
+                height (int): height of the rectangle
+        """
+        if isinstance(width, int):
+            if width < 0:
+                raise ValueError("width must be >= 0")
+            self.__width = width
+        else:
+            raise TypeError("width must be an integer")
+
+        if isinstance(height, int):
+            if height < 0:
+                raise ValueError("height must be >= 0")
+            self.__height = height
+        else:
+            raise TypeError("height must be an integer")
 
     @property
     def width(self):
-        """returns width of rectangle"""
+        """
+            getter function for private attribute width
+            Retruns: width
+        """
         return self.__width
 
     @width.setter
     def width(self, value):
-        """width validation and setter"""
-        if type(value) is not int:
+        """
+            setter function for private attribute width
+            Args:
+                value (int): new width value
+        """
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
-        if value < 0 is True:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """returns height"""
+        """
+            getter function for private attribute height
+            Returns: height
+        """
         return self.__height
 
     @height.setter
     def height(self, value):
-        """height validation and setter"""
-        if type(value) is not int:
+        """
+            setter function for the private attribute height
+            Args:
+                value (int): new height value
+        """
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
-        if value < 0 is True:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
