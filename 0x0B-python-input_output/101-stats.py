64d090d78a883c939253c4ef743464f726be01d1
DennisWanjeri <shikandadennis07@gmail.com>
2021-06-01 20:15:57 +0300

    pep8

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 """reads stdin line by line and computes metrics"""
 
+
 def print_stats(size, status_codes):
     """prints accumulated stats"""
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
 
+
 if __name__ == "__main__":
     import sys
-
-
     size = 0
     status_codes = dict()
     valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
