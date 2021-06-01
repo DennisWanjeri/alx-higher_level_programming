5d0303eb400b3c43efe97b901d549fa4e48321b5
DennisWanjeri <shikandadennis07@gmail.com>
2021-06-01 08:24:07 +0000

    documentation

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
+"""Square class"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size):
+        """constructor"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
+"""module Square"""
 Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
     """Square class"""
     def __init__(self, size):
+        """constructor"""
         self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
     def __str__(self):
+        """print representation"""
         string = "[{}] {}/{}".format("Square", self.__size, self.__size)
         return string
