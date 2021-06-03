5495fa2186698b77dc9f28068cc4d1d56362e99e
root <DennisWanjeri>
2021-06-03 09:34:41 +0000

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Class square module"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Square class definition"""
+    def __init__(self, size, x=0, y=0, id=None):
+        """constructor"""
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """get the value of size, in a square width == height"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """set and validate size"""
+        self.width = value
+        self.height = value
+
+    def update(self, *args, **kwargs):
+        """assigns attributes"""
+        if args and len(args) != 0:
+            i = 0
+            for arg in args:
+                if i == 0:
+                    if arg is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif i == 1:
+                    self.size = arg
+                elif i == 2:
+                    self.x = arg
+                elif i == 3:
+                    self.y = arg
+                i += 1
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "size":
+                    self.size = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    def __str__(self):
+        """print representation"""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
