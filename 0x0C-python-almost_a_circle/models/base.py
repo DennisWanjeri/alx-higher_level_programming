0ab3254214468170860d67d3edb0ca7bf11bddca
root <DennisWanjeri>
2021-06-03 05:27:36 +0000

    almost a circle

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""defines class Base"""
+
+
+class Base:
+    """Class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
