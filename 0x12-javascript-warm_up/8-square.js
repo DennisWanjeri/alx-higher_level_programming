a3032b342fba1433fe8766e936a2d2ffc4f92efb
DennisWanjeri <shikandadennis07@gmail.com>
2021-08-03 01:43:59 +0000

    camel case

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 const size = Math.floor(Number(process.argv[2]));
 if (isNaN(size)) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
-    for (let row = 0; row < size; row++) {
-	let row_str = '';
-	for (let c = 0; c < size; c++) row_str += 'X';
-	console.log(row_str);
-    }
+  for (let row = 0; row < size; row++) {
+    let rowstr = '';
+    for (let c = 0; c < size; c++) rowstr += 'X';
+    console.log(rowstr);
+  }
 }
