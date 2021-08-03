61b2195559a48874395890c1d9b74e8468cf439b
DennisWanjeri <shikandadennis07@gmail.com>
2021-08-03 02:11:21 +0000

    code fix

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
-for (const language of ['C is fun', 'Python is cool', 'Javascript is amazing']) {
-  console.log(language);
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
 }
