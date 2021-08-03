fc214e1c39f6981005651e2251b5526783397c18
DennisWanjeri <shikandadennis07@gmail.com>
2021-08-03 15:08:37 +0000

    semistandard errors correction

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
-    let x = 0;
-    for (let i = 0; i < list.length; i++) {
-	if (list[i] === searchElement)
-	{
-	    x += 1;
-	}
+  let x = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      x += 1;
     }
-    return x;
+  }
+  return x;
 };
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    let newlist = [];
-    for (let i = list.length - 1, j = 0; i >= 0; i--, j++)
-    {
-	newlist[j] = list[i];
-    }
-    return newlist;
+  const newlist = [];
+  for (let i = list.length - 1, j = 0; i >= 0; i--, j++) {
+    newlist[j] = list[i];
+  }
+  return newlist;
 };
