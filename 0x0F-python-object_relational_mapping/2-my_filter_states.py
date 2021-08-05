b80052215da60b6632d80901c84f602fbb75e7a0
DennisWanjeri <shikandadennis07@gmail.com>
2021-08-05 16:10:38 +0000

    my filters

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+# takes an arguement and displays all states that match the arguement
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * \
+                      FROM `states` \
+                      WHERE BINARY `name` = '{}'".format(sys.argv[4]))
+    [print(state) for state in cursor.fetchall()]
