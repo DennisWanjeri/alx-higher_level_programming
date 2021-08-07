31e00a00b374a874957d11d84c0e2fa375eacb8e
DennisWanjeri <shikandadennis07@gmail.com>
2021-08-07 18:59:37 +0000

    filter states

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-# displays all contents of states table with protection from SQL injection
+""" displays all contents of states table
+ with protection from SQL injection """
 import sys
 import MySQLdb
 
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    db = MySQLdb.connect(user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `states`")
-    [print(state) for state in cursor.fetchall() if state[1] == sys.argv[4]]
+    fetch = cursor.fetchall()
+    [print(state) for state in fetch if state[1] == sys.argv[4]]
