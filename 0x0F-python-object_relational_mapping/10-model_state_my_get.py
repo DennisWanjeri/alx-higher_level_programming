a6395fd8baed05746ed0cf790c59284bae3129c0
DennisWanjeri <shikandadennis07@gmail.com>
2021-08-07 19:31:09 +0000

    pep8

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# prints the state object with the name passed as an arguement
+""" prints the state object with the name passed as an arguement"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
