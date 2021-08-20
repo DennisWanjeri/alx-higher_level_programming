0441624557a7a1389b334f80793eed94781b1d7c
DennisWanjeri <shikandadennis07@gmail.com>
2021-08-20 13:11:11 +0000

    api

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""takes in a URL, sends a request to it, displays response body"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
