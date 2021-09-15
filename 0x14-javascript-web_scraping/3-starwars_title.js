475727ba3ec7c43950e19d392af5f3c4e1249ff6
DennisWanjeri <shikandadennis07@gmail.com>
2021-09-15 11:24:27 +0000

    starwars title

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,6 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-let url = 'http://swapi.co/api/films/' + process.argv[2];
-request(url, function (error, response, body) {
-  console.log(error || JSON.parse(body).title);
-});
+
+const movie_id = process.argv[2];
+
+if (parseInt(movie_id) < 8) {
+    const url = 'https://swapi-api.hbtn.io/api/films/' + movie_id;
+    request(url, function (err, response, body) {
+	if (err) {
+	    return console.log(err);
+	}
+	console.log(JSON.parse(body).title);
+    });
+}
